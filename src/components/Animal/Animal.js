b198fcae4289515e75ed3570a2bad7ffb55ffebe
Dillon Brock <mdillon1297@gmail.com>
2022-09-12 12:56:25 -0700

    built out animal component

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`}/>
+      <p className="name">{props.name}</p>
+      <p>{props.says}</p>
+    </div>
+  );
 }
